669a2fbe54379efba3c0746a257cf7cf9e17b949
Thanh Tung <tung@codelynx.io>
2021-12-05 14:38:05 +0700

    add zsh install

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -3,18 +3,23 @@
 # Install grep
 sudo apt install ripgrep
 
-# Install NeoVim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-sudo chmod u+x nvim.appimage
-./nvim.appimage
-
-# Ubuntu
-# sudo apt-get install software-properties-common
-
-# Python
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+# g++
+sudo apt install g++ -y
 
 # VimPlug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# Python
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+# Install NeoVim
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+
+# Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+source ~/.zshrc
