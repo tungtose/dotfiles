ec75282b3c648ed02f89007d8eed4dcaa85d6cb3
Thanh Tung <tung@codelynx.io>
2021-12-07 16:35:23 +0700

    update conf

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -11,13 +11,13 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Python
-sudo apt-get install python-dev python-pip python3-dev python3-pip -y
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip
 
 # Install NeoVim
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo apt-get update -y
+sudo apt-get install -y neovim
 
 # Zsh
 sudo apt install zsh -y
@@ -26,6 +26,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 source ~/.zshrc
 
 # Exa
-sudo add-apt-repository ppa:spvkgn/exa
+sudo add-apt-repository ppa:spvkgn/exa -y
 sudo apt update -y
 sudo apt install exa -y
