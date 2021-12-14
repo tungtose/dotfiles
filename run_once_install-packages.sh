211953e2d4338c16242f14fe727737a579d7ca26
Thanh Tung <tung@codelynx.io>
2021-12-14 14:58:20 +0700

    install zsh unattended mode

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -3,15 +3,14 @@
 #Exa
 sudo add-apt-repository ppa:spvkgn/exa -y
 sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:neovim-ppa/stable
-sudo apt update -y
-sudo apt install exa -y
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update -y
+sudo apt-get install exa -y
 # Install NeoVim
 sudo apt-get install -y neovim
 
 # Install grep
-sudo apt install ripgrep
+sudo apt-get install ripgrep -y
 
 # g++
 sudo apt install g++ -y
@@ -21,12 +20,12 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Python
-sudo apt-get install python-dev python-pip python3-dev python3-pip -y
+# sudo apt-get install python-dev python-pip python3-dev python3-pip -y
 
 
 # Zsh
 sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 source ~/.zshrc
 
