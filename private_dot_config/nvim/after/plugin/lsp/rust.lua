4add55f77a9922ad0e857d65e3da754f8779e7dd
tungto <tungtose@gmail.com>
2022-12-21 01:25:22 +0700

    update lsp rust

--- private_dot_config/nvim/after/plugin/lsp/rust.lua
+++ private_dot_config/nvim/after/plugin/lsp/rust.lua
@@ -19,7 +19,7 @@ require'lspconfig'.rust_analyzer.setup {
 local opts = {
   tools = {
     autoSetHints = true,
-    hover_with_actions = true,
+    -- hover_with_actions = true,
     runnable = {
       use_telescope = true
     },
@@ -63,6 +63,12 @@ local opts = {
   },
 
   server = {
+    on_attach = function(_, bufnr)
+     -- Hover actions
+     vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+     -- Code action groups
+     vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end, 
     settings = {
       ["rust-analyzer"] = {
         checkOnSave = {
