1a1414c30bfc49bf886b035e6f91728ea93066a9
Tung To <tungtose@gmail.com>
2025-08-22 13:10:53 +0700

    add fish

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+end
+abbr -a -g vpnup 'wg-quick up wg0'
+abbr -a -g vpndown 'wg-quick down wg0'
+abbr -a -g v 'nvim'
+
+
+abbr -a -g gl 'git pull'
+abbr -a -g gco 'git checkout'
+
